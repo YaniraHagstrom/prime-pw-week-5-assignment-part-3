e133e6a64d39d4a5bf7dc61d808627809dc02592
YaniraHagstrom <yanirahagstrom@gmail.com>
2022-08-14 13:39:37 -0500

    Added six albums to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,31 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection (title, artist, yearPublished) {
+    const album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    collection.push(album);
+    return album;
+}
+
+console.log(addToCollection("And Justice for All", "Metallica", "1988"));
+console.log(addToCollection("Surfer Rosa", "Pixies", "1989"));
+console.log(addToCollection("Green", "R.E.M", "1987"));
+console.log(addToCollection("Nothing's Shocking", "Jane's Addiction", "1985"));
+console.log(addToCollection("Spirit of Eden", "Talk Talk", "1984"));
+console.log(addToCollection("Vivid", "Living Colour", "1990"));
+
+console.log(collection);
+
+let artists = [];
+
+function findByArtist (artist) {
+    for (let i = 0; i < collection.length; i ++){
+        if (album.artist === )
+    }
+
+}
