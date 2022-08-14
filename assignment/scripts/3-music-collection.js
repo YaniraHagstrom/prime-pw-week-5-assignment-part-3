2ff77c8ce81a4b3fa3c28542415d1087e7cf0aff
YaniraHagstrom <yanirahagstrom@gmail.com>
2022-08-14 14:08:52 -0500

    Added the findByArtist function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -21,11 +21,22 @@ console.log(addToCollection("Vivid", "Living Colour", "1990"));
 
 console.log(collection);
 
-let artists = [];
+let matchingArtists = [];
 
-function findByArtist (artist) {
-    for (let i = 0; i < collection.length; i ++){
-        if (album.artist === )
+function findByArtist (input) {
+    for (let i = 0; i < collection.length; i++){
+        //check is the input string matches the artist parameter in the current index
+        if (collection[i].artist == input){
+            // add the object from this index to the empty array "matchingArtists"
+            matchingArtists.push(collection[i]);
+            console.log(` This artist is in the collection: ${collection[i].artist}`);
+        }
+        else {
+            console.log(` This artist is not in the collection: ${collection[i].artist}`);
+        }
     }
-
+    return matchingArtists;
 }
+
+console.log("Should return an empty array:", findByArtist("Spice Girls"));
+console.log("Should return an object with a matching artist:", findByArtist('Living Colour'));
